ecc2347beb050d1166713598cfe60c3bdf6a9eef
bd_76 <barrycdonnelly@gmail.com>
2017-04-12 15:45:21 +0100

    First revision

---
+++ LambdaAnatomy/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LambdaAnatomy
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            /*ClickMeButton.Click += 
+                ( s, e) => MessageBox.Show("Hello World");*/
+
+        }
+
+
+        /* private void ClickMeButton_Click(object sender, RoutedEventArgs e)
+         {
+             MessageBox.Show("Hello World");
+         }*/
+
+        // 1: Named delegate
+        /*private void ClickMeButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello World");
+        }*/
+
+
+        // 2: Anonymouse delegate
+        /* ClickMeButton.Click += 
+                 delegate (object sender, RoutedEventArgs e)
+                 {
+                     MessageBox.Show("Hello World");
+                 };*/
+
+        // 3 To turn above into a lambda expression
+        // remove delegate and add goto operator =>
+        /*(object sender, RoutedEventArgs e) =>
+                {
+                    MessageBox.Show("Hello World");
+                };*/
+
+        // Can remove object and RoutedEventArgs , parameter typed inference
+    }
+
+
+}
